1115fdd789fd970b13d286d28c88b1e2a499e0fc
DrappEtienne <etienne.drapp@epsi.fr>
2021-04-29 11:23:35 +0200

    Ajout commentaire, ajout erreur, ajout création dossier

--- src/main/java/com/msprb3/App.java
+++ src/main/java/com/msprb3/App.java
@@ -5,7 +5,7 @@ import java.io.*;
 
 public class App {
     public static void main(String[] argsv) throws IOException {
-        Export.genererAccueil();
-        Export.genererAgent();
+        Export.genererAccueil();  //On appelle la génératon de la page index
+        Export.genererAgent();    //On appelle la génération des pages agents
     }
 }
